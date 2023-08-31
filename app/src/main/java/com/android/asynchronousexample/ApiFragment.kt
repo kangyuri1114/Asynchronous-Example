9cd2e8cf2fc98ae4ce593a318822d1ba85fae66c
kangyuri1114 <yuri3334@gmail.com>
2023-09-01 02:05:17 +0900

    프래그먼트 두개 각각 실행 성공 - 비동기

--- app/src/main/java/com/android/asynchronousexample/ApiFragment.kt
+++ app/src/main/java/com/android/asynchronousexample/ApiFragment.kt
@@ -1,23 +1,22 @@
 package com.android.asynchronousexample
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiFragment : Fragment() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: NewsAdapter
+    private val apiKey = "66416bcc4f96467b82bd61929f14df54"
+    private val country = "kr"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://newsapi.org/")
@@ -36,31 +35,26 @@ class ApiFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        recyclerView = view.findViewById(R.id.newsRecyclerView)
-        adapter = NewsAdapter()
+        val newsTextView = view.findViewById<TextView>(R.id.newsTextView)
 
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = adapter
-
-        val apiKey = "NEWS_API_KEY"
-        val country = "kr"
-
-        GlobalScope.launch(Dispatchers.IO) {
-            try {
-                val response = newsApiService.getTopHeadlines(apiKey, country).execute()
+        val call = newsApiService.getTopHeadlines(apiKey, country)
+        call.enqueue(object : Callback<NewsResponse> {
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
                 if (response.isSuccessful) {
                     val newsResponse = response.body()
-                    withContext(Dispatchers.Main) {
-                        newsResponse?.articles?.let {
-                            adapter.setData(it)
-                        }
-                    }
+                    // 응답 처리 로직
+                    Log.d("isSuccessful", "$newsResponse")
+                    newsTextView.text = newsResponse.toString()
                 } else {
                     // 에러 처리 로직
+                    val errorBody = response.errorBody()?.string()
+                    Log.d("isSuccessful", "Fail: $errorBody")
                 }
-            } catch (e: Exception) {
-                // 예외 처리 로직
             }
-        }
+
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                // 실패 처리 로직
+            }
+        })
     }
 }
